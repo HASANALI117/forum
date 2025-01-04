d53a3a6e361c03af10da6106cfc023daf4fda661
Ahmed <a7med3365@gmail.com>
2025-01-04 21:55:51 +0300

    Add OnlineUsersHandler to retrieve active users; implement endpoint for online users

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
 
 	db "forum/pkg/db"
 	handlers "forum/pkg/handlers"
 	ws "forum/pkg/websockets"
 )
+
+func OnlineUsersHandler(h *ws.Hub) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users := h.GetOnlineUsers()
+		data, _ := json.Marshal(users)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	}
+}
 
 func main() {
 
@@ -28,15 +38,18 @@ func main() {
 	http.HandleFunc("/api/register", handlers.RegisterHandler(dbWrapper))
 	http.HandleFunc("/api/login", handlers.LoginHandler(dbWrapper))
 	http.HandleFunc("/api/logout", handlers.LogoutHandler(dbWrapper))
-
+	
 	http.HandleFunc("/api/posts", handlers.GetPostsHandler(dbWrapper))
 	http.HandleFunc("/api/create_post", handlers.CreatePostHandler(dbWrapper))
 	http.HandleFunc("/api/post/", handlers.GetPostByIDHandler(dbWrapper))
 	http.HandleFunc("/api/comments", handlers.GetCommentsHandler(dbWrapper))
 	http.HandleFunc("/api/create_comment", handlers.CreateCommentHandler(dbWrapper))
-
+	
 	http.HandleFunc("/api/get_messages", handlers.GetMessagesHandler(dbWrapper))
+	http.HandleFunc("/api/all_messages", handlers.GetAllMessagesHandler(dbWrapper))
 	http.HandleFunc("/api/users_list", handlers.GetUsersListHandler(dbWrapper))
+	http.HandleFunc("/api/current_user", handlers.CurrentUserHandler(dbWrapper))
+	http.HandleFunc("/api/online_users", OnlineUsersHandler(hub))
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.ServeWs(hub, dbWrapper, w, r)
